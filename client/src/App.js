c1451e018a9e7c726227b5aa175c4a26b8cb4daa
Thomas Martin <tmartin.dev101010@gmail.com>
2019-01-22 12:19:09 -0700

    Basic MERN structure in place.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+     <div>
+       Ready to rock and roll!!
+     </div> 
+    );
+  }
+}
+
+export default App;
