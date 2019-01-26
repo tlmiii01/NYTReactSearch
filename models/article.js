57586929b2bf987bd7ce11923378e5b41104b82f
Thomas Martin <tmartin.dev101010@gmail.com>
2019-01-25 20:38:06 -0700

    Now pulling the article ID and using in Mongo database.

--- models/article.js
+++ models/article.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
     title: {
         type: String,
         required: true
